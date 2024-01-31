bd017aa113d5dae8591d0c1e6f6762a521be80f5
Andrey588255 <orlovskiyswat@gmail.com>
2024-01-31 20:37:01 +0200

    1

---
+++ src/main/java/com/hajdanka/Foo.java
@@ -0,0 +1,30 @@
+package com.hajdanka;
+
+
+import java.util.concurrent.CountDownLatch;
+
+public class Foo {
+    private CountDownLatch countDownLatch1;
+    private CountDownLatch countDownLatch2;
+
+    public Foo(){
+        this.countDownLatch1 = new CountDownLatch(1);
+        this.countDownLatch2 = new CountDownLatch(1);
+    }
+
+    public void first(Runnable runnable){
+        System.out.print("first");
+        countDownLatch1.countDown();
+    }
+
+    public void second(Runnable runnable) throws InterruptedException {
+        countDownLatch1.await();
+        System.out.print("second");
+        countDownLatch2.countDown();
+    }
+
+    public void third(Runnable runnable) throws InterruptedException {
+        countDownLatch2.await();
+        System.out.print("third");
+    }
+}
---
+++ src/main/java/com/hajdanka/Main.java
@@ -0,0 +1,30 @@
+package com.hajdanka;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+    public static void main(String [] args) throws InterruptedException {
+        Foo foo = new Foo();
+
+        CompletableFuture.runAsync(() -> {
+            foo.first(new Thread());
+        });
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                foo.second(new Thread());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                foo.third(new Thread());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread.sleep(2000);
+    }
+}
